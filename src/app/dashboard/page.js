2d0743c03ada297034cbb0fdaf7d257b01d33430
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-11-23 12:09:04 +0800

    try

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import Card from "../components/Card";
-import DashboardHeader from "../components/DashboardHeader";
 import ServiceRequestChart from "../components/ServiceRequestChart";
-import ServiceEfficiencyChart from "../components/Chart2";
 import Chart2 from "../components/Chart2";
 import FinanceChart from "../components/FinanceChart";
 import Calendar from "../components/EventCalendar";
@@ -10,39 +8,59 @@ import Announcement from "../components/Announcement";
 
 export default function page() {
   return (
-    <div className="h-screen flex">
-      <Link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css
-"
-      />
-      <div className="w-1/6 md:[8%] lg:[16%] xl:w-[14%]">left</div>
-      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]">
-        <div className="p-4 flex gap-4 flex-col md:flex-row">
-          <div className="w-full lg:w-2/3 flex flex-col gap-8">
-            <div className="flex gap-4 justify-between flex-wrap">
+    <div className="min-h-screen bg-[#F7F8FA]">
+      <Link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
+      <div className="p-4 w-full">
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* Main Content - Left Side */}
+          <div className="w-full lg:w-3/4 flex flex-col gap-6">
+            {/* Cards Grid */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
               <Card type="patient" />
               <Card type="sales" />
               <Card type="appointments" />
               <Card type="staff" />
             </div>
-            <div className="flex gap-4 flex-col lg:flex-row">
-              <div className="w-full lg:w-1/3 h-[450px]">
+
+            {/* Charts Section */}
+            <div className="flex flex-col lg:flex-row gap-4">
+              <div className="w-full lg:w-2/5 bg-white rounded-lg shadow-sm h-[500px] p-4">
                 <ServiceRequestChart />
               </div>
-              <div className="w-full lg:w-2/3 h-[450px]">
+              <div className="w-full lg:w-3/5 bg-white rounded-lg shadow-sm h-[500px] p-4">
                 <Chart2 />
               </div>
             </div>
-            <div className="w-full h-[500px]">
+
+            {/* Finance Chart */}
+            <div className="w-full bg-white rounded-lg shadow-sm h-[500px] p-4">
               <FinanceChart />
             </div>
           </div>
-          <div className="w-full lg:w-1/3 flex flex-col gap-8">
-            <Calendar />
-            <Announcement />
+
+          {/* Sidebar - Right Side */}
+          <div className="w-full lg:w-1/4 flex flex-col gap-6">
+            {/* Calendar Container */}
+            <div className="bg-white rounded-lg shadow-sm">
+              <div className="p-4 h-full">
+                <h3 className="text-lg font-semibold mb-4">Calendar</h3>
+                <div className="h-[600px] overflow-y-auto custom-scrollbar">
+                  <Calendar />
+                </div>
+              </div>
+            </div>
+
+            {/* Announcement Container */}
+            <div className="bg-white rounded-lg shadow-sm">
+              <div className="p-4">
+                <div className="min-h-[300px] overflow-y-auto custom-scrollbar">
+                  <Announcement />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </div>      
     </div>
   );
 }
